4017dbf53b8355d3bd0ce40e43e28d82b8d72f30
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-01-07 22:51:22 +0200

    Added more code for weight network init

--- lib/braintonic.ex
+++ lib/braintonic.ex
@@ -3,21 +3,21 @@ defmodule BrainTonic do
   A neural network
   """
 
-  @default_options %{
+  @default_parameters %{
+    hidden_layers_sizes: [1],
+    hidden_layers_number: 1,
     input_size: 1,
-    number_of_hidden_layers: 1,
-    hidden_layer_sizes: [1],
-    output_layer_size: 1,
-    learning_rate: 0.5
+    learning_rate: 0.5,
+    output_layer_size: 1
   }
 
   @doc """
   Initalizez the neural network
   """
   @spec initialize(map) :: pid
-  def initialize(options) do
-    settings = Map.merge(@default_options, options)
-    spawn fn -> network_loop(settings) end
+  def initialize(parameters) do
+    state = Map.merge(@default_parameters, parameters) |> initialize_state
+    spawn fn -> network_loop(state) end
   end
 
   @doc """
@@ -52,9 +52,6 @@ defmodule BrainTonic do
     file
   end
 
-  @doc """
-  The neural network loop
-  """
   @spec network_loop(map) :: no_return
   defp network_loop(state) do
     receive do
@@ -65,5 +62,54 @@ defmodule BrainTonic do
         send caller, input
         network_loop(state)
     end
+  end
+
+  @spec initialize_state(map) :: map
+  defp initialize_state(parameters) do
+    layers = parameters[:hidden_layers_number] + 1
+    hidden_sizes = parameters[:hidden_layers_sizes]
+    output_size = parameters[:output_layer_size]
+    column_sizes = List.insert_at(hidden_sizes, -1, output_size)
+    weights = initialize_network(layers, column_sizes)
+    biases = initialize_layer(layers)
+    %{
+      weights: weights,
+      biases: biases,
+      activations: {}
+    }
+  end
+
+  @spec initialize_network(pos_integer, [any(),...]) :: tuple
+  defp initialize_network(rows, column_sizes) do
+    initialize_layers({}, rows, column_sizes)
+  end
+
+  @spec initialize_layers(tuple, pos_integer, list) :: tuple
+  defp initialize_layers(layers, 0, _sizes) do
+    layers
+  end
+
+  defp initialize_layers(layers, remaining, sizes) do
+    [size | other_sizes] = sizes
+    new_layer = initialize_layer(size)
+    new_layers = Tuple.append(layers, new_layer)
+    initialize_layers(new_layers, remaining - 1, other_sizes)
+  end
+
+  @spec initialize_layer(pos_integer) :: tuple
+  defp initialize_layer(count) do
+    initialize_tuple({}, count - 1)
+  end
+
+  @spec initialize_tuple(tuple, pos_integer) :: tuple
+  defp initialize_tuple(accumulator, 0) do
+    accumulator
+  end
+
+  defp initialize_tuple(accumulator, count) do
+    # TODO: make this actually random
+    value = :random.uniform
+    new_acc = Tuple.append(accumulator, value)
+    initialize_tuple(new_acc, count - 1)
   end
 end
