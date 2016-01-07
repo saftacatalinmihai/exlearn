2654db0bbff8311cda48ba686cad5e0324cf6ca9
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-01-07 20:32:28 +0200

    Added some initial frame

--- lib/braintonic.ex
+++ lib/braintonic.ex
@@ -3,11 +3,36 @@ defmodule BrainTonic do
   A neural network
   """
 
+  @default_options %{
+    input_size: 1,
+    number_of_hidden_layers: 1,
+    hidden_layer_sizes: [1],
+    output_layer_size: 1,
+    learning_rate: 0.5
+  }
+
   @doc """
   Initalizez the neural network
   """
-  @spec initialize(tuple) :: tuple
+  @spec initialize(map) :: pid
   def initialize(options) do
-    {}
+    settings = Map.merge(@default_options, options)
+    spawn fn -> settings end
+  end
+
+  @doc """
+  Trains the neural network
+  """
+  @spec train(any, pid) :: any
+  def train(value, pid) do
+    send pid, {:train, value}
+  end
+
+  @doc """
+  Makes a prediction
+  """
+  @spec predict(any, pid) :: any
+  def predict(input, pid) do
+    send pid, {:predict, input}
   end
 end
