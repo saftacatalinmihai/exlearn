242fd9f43e32a56bab93d0aae4c7e5775a241afb
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-01-31 13:04:57 +0200

    Fixed a failing test

--- test/builder_test.exs
+++ test/builder_test.exs
@@ -1,17 +1,15 @@
 defmodule BuilderTest do
   use ExUnit.Case
 
-  alias BrainTonic.NeuralNetwork.Builder, as: B
+  alias BrainTonic.NeuralNetwork.Builder
 
-  test "initialize_neural_network return a map" do
+  test "initialize return a map" do
     parameters = %{
       hidden_layers_sizes: [2, 3],
       hidden_layers_number: 2,
-      input_size: 4,
-      learning_rate: 0.5,
       output_layer_size: 5
     }
-    result = parameters |> B.initialize_neural_network
+    result = parameters |> Builder.initialize
     assert result |> is_map
   end
 end
