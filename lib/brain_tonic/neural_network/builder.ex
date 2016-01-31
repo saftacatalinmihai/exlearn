de7238db756110ea5105c37f8acce45851a56ed4
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-01-31 09:28:18 +0200

    Replaced tuples with lists

--- lib/brain_tonic/neural_network/builder.ex
+++ lib/brain_tonic/neural_network/builder.ex
@@ -8,19 +8,19 @@ defmodule BrainTonic.NeuralNetwork.Builder do
     layers = parameters[:hidden_layers_number]
     hidden_sizes = parameters[:hidden_layers_sizes]
     output_size = parameters[:output_layer_size]
-    column_sizes = List.insert_at(hidden_sizes, -1, output_size)
+    column_sizes = hidden_sizes ++ [output_size]
     weights = initialize_network(layers + 1, column_sizes)
     biases = initialize_layer(layers)
     %{
       weights: weights,
       biases: biases,
-      activations: {}
+      activations: []
     }
   end
 
   @spec initialize_network(pos_integer, [any(),...]) :: tuple
   defp initialize_network(rows, column_sizes) do
-    initialize_layers({}, rows, column_sizes)
+    initialize_layers([], rows, column_sizes)
   end
 
   @spec initialize_layers(tuple, pos_integer, list) :: tuple
@@ -31,26 +31,26 @@ defmodule BrainTonic.NeuralNetwork.Builder do
   defp initialize_layers(layers, remaining, sizes) do
     [size | other_sizes] = sizes
     new_layer = initialize_layer(size)
-    new_layers = Tuple.append(layers, new_layer)
+    new_layers = layers ++ [new_layer]
     initialize_layers(new_layers, remaining - 1, other_sizes)
   end
 
   @spec initialize_layer(pos_integer) :: tuple
   defp initialize_layer(count) do
-    initialize_tuple({}, count - 1)
+    initialize_list([], count - 1)
   end
 
-  @spec initialize_tuple(tuple, pos_integer) :: tuple
-  defp initialize_tuple(accumulator, -1) do
+  @spec initialize_list(list, pos_integer) :: list
+  defp initialize_list(accumulator, -1) do
     accumulator
   end
 
-  defp initialize_tuple(accumulator, count) do
+  defp initialize_list(accumulator, count) do
     # TODO: make this more random
     << a :: 32, b :: 32, c :: 32 >> = :crypto.rand_bytes(12)
     :random.seed(a,b,c)
     value = :random.uniform
-    new_acc = Tuple.append(accumulator, value)
-    initialize_tuple(new_acc, count - 1)
+    new_acc = accumulator ++ [value]
+    initialize_list(new_acc, count - 1)
   end
 end
