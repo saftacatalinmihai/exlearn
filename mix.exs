ffd12ac83f0a0f896a59ebc7d7cc10f93dbf0485
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-07-11 15:11:46 +0300

    Corrects first version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExLearn.Mixfile do
   def project do
     [
       app:             :ExLearn,
-      version:         "0.0.1",
+      version:         "0.1.0",
       elixir:          "1.3.1",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
