9dc4e65eb4972a78932096bab8b026443031b3b4
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-07-11 22:41:53 +0300

    Progress on calculating activity with layers

--- lib/exlearn/neural_network/forwarder.ex
+++ lib/exlearn/neural_network/forwarder.ex
@@ -41,52 +41,31 @@ defmodule ExLearn.NeuralNetwork.Forwarder do
   """
   @spec forward_for_activity([number], map) :: [map]
   def forward_for_activity(batch, state) do
-    batch
-      |> full_network(state)
-      |> calculate_activity([])
+    %{network: %{layers: layers}} = state
+
+    calculate_activity(batch, layers, [])
   end
 
-  @spec calculate_activity(map, [map]) :: map
-  defp calculate_activity(network, activities) do
-    case network do
-      %{weights: [_|[]]} ->
-        Matrix.transpose(activities)
-          |> Enum.map(fn (element) ->
-            [%{output: [out]}|_] = element
-            result = Enum.reverse(element)
-
-            %{activity: result, output: out}
-          end)
-      %{weights: [batch, w|ws], biases: [b|bs], activity: [a|as]} ->
-        %{function: f, derivative: d} = a
-
-        activity = Enum.map(batch, fn (sample) ->
-          input  = Matrix.dot(sample, w) |> Matrix.add(b)
-          output = input |> Matrix.apply(f)
-
-          %{derivative: d, input: input, output: output}
-        end)
-
-        output      = Enum.map(activity, fn (element) -> element[:output] end)
-        new_network = %{weights: [output|ws], biases: bs, activity: as}
-
-        calculate_activity(new_network, [activity|activities])
-    end
+  defp calculate_activity(output, [], activities) do
+    %{activity: activities, output: output}
   end
 
-  # Prepends the input as a matrix to the weight list
-  @spec full_network([[number]], map) :: map
-  defp full_network(batch, state) do
-    %{network: network} = state
-    %{weights: weights} = network
+  defp calculate_activity(batch_input, [layer|rest], activities) do
+    %{
+      activity: %{function: function, derivative: derivative},
+      biases:   biases,
+      weights:  weights
+    } = layer
 
-    input_list = Enum.map(batch, fn (sample) ->
-      case sample do
-        {input, _} -> [input]
-        input when is_list(input) -> [input]
-      end
+    activity = Enum.map(batch_input, fn (sample) ->
+      input  = Matrix.dot(sample, weights) |> Matrix.add(biases)
+      output = Matrix.apply(input, function)
+
+      %{derivative: derivative, input: input, output: output}
     end)
 
-    put_in(network, [:weights], [input_list|weights])
+    batch_output = Enum.map(activity, fn (element) -> element[:output] end)
+
+    calculate_activity(batch_output, rest, [activity|activities])
   end
 end
