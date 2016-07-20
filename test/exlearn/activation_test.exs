a2412b92106cb9cda0b10cee0bcacc5c1c01aa3a
mihai <saftacatalinmihai@gmail.com>
2016-07-20 11:08:24 +0300

    Added failing test for overflowing softmax function

--- test/exlearn/activation_test.exs
+++ test/exlearn/activation_test.exs
@@ -247,6 +247,21 @@ defmodule ActivationTest do
     assert derivative.([all])       == expected_from_derivative
   end
 
+  test "#determine the softmax pair with no overflow" do
+    all      = [-1.5, 880, 0.3, 3]
+    argument = 880
+
+    expected_from_function   = 0.8778671136285249
+    # expected_from_derivative = [[1.5, -0.2, -0.29999999999999993, -3.0]]
+
+    setup = :softmax
+
+    %{function: function, derivative: derivative} = Activation.determine(setup)
+
+    assert function.(argument, all) == expected_from_function
+    # assert derivative.([all])       == expected_from_derivative
+  end
+
   test "#determine the sinc pair" do
     setup = :sinc
 
